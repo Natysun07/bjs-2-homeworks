91c03e39d75d8ad160da537cb275b05aa6c89a49
Natysun07 <127549725+Natysun07@users.noreply.github.com>
2023-08-30 12:25:47 +0300

    Выполнение первого задания

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,33 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let d = Math.pow(b, 2) - 4 * a * c;
+	if (d < 0) {
+		return [];
+	} else if (d === 0) {
+		let root = -b / (2 * a);
+		return [root];
+	} else {
+		let root1 = (-b + Math.sqrt(d)) / (2 * a);
+		let root2 = (-b - Math.sqrt(d)) / (2 * a);
+		return (root1, root2);
+	}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	if (typeof percent !== 'number' || typeof contribution !== 'number' || typeof amount !== 'number' || typeof countMonths !== 'number') {
+		return false;
+
+		const monthlyPercent = percent / 100 / 12;
+
+		const creditAmount = amount - contribution;
+
+		const monthlyPayment = creditAmount * (monthlyPercent + (monthlyPercent / (Math.pow(1 + monthlyPercent, countMonths) - 1)));
+
+		const totalPayment = monthlyPayment * countMonths;
+
+		const roundedTotalPayment = Math.round(totalPayment * 100) / 100;
+
+		return roundedTotalPayment;
+	}
 }
