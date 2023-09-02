9e5cbe40428963074992ff60eada193e9d9def23
Natysun07 <127549725+Natysun07@users.noreply.github.com>
2023-09-02 19:36:05 +0300

    Третье задание полностью выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,20 +1,20 @@
 function compareArrays(arr1, arr2) {
-    if (arr1.length !== arr2.length) {
-      return false;
-    }
-    return arr1.every((element, index) => element === arr2[index]);
+  if (arr1.length !== arr2.length) {
+    return false;
   }
+  return arr1.every((element, index) => element === arr2[index]);
+}
   
-  function getUsersNamesInAgeRange(users, gender) {
-    const filteredUsers = users.filter(user => user.gender === gender);
+function getUsersNamesInAgeRange(users, gender) {
+  const filteredUsers = users.filter(user => user.gender === gender);
     
-    if (filteredUsers.length === 0) {
-      return 0;
-    }
-    
-    const ages = filteredUsers.map(user => user.age);
-    const totalAge = ages.reduce((sum, age) => sum + age, 0);
-    const averageAge = totalAge / filteredUsers.length;
-    
-    return averageAge;
+  if (filteredUsers.length === 0) {
+    return 0;
   }
+    
+  const ages = filteredUsers.map(user => user.age);
+  const totalAge = ages.reduce((sum, age) => sum + age, 0);
+  const averageAge = totalAge / filteredUsers.length;
+    
+  return averageAge;
+}
